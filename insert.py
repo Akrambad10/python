1449710653774cb5321a0dfd504a6e6d8327e53d
Akrambad10 <mkuria301@gmail.com>
2024-02-12 13:20:14 +0300

    4commit

--- insert.py
+++ insert.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("example.db")
 print("opened database successfully")
 
-conn.execute("INSERT INTO EMPLOYEES VALUES (1,'FAITH KARIMI',23,345000.00)")
+conn.execute("INSERT INTO EMPLOYEES VALUES (1,'FAITH KARIMI',23,345000.00)")git
 conn.execute("INSERT INTO EMPLOYEES VALUES (2,'BOB KURIA',23,15000.00)")
 conn.execute("INSERT INTO EMPLOYEES VALUES (3,'JANE MUTHONI',27,40000.00)")
 conn.execute("INSERT INTO EMPLOYEES VALUES (4,'MARY ANNE',25,945000.00)")
